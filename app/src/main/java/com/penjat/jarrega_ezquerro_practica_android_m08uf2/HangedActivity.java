3f56671979f00acb3c7a356a6bbc6ca63b937516
Destructor266 <jarmastertruck@gmail.com>
2023-04-25 17:27:19 +0200

    Added support for firebase, created main activity and hanged activity with interconnection between them

---
+++ app/src/main/java/com/penjat/jarrega_ezquerro_practica_android_m08uf2/HangedActivity.java
@@ -0,0 +1,13 @@
+package com.penjat.jarrega_ezquerro_practica_android_m08uf2;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HangedActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hanged);
+    }
+}
--- app/src/main/java/com/penjat/jarrega_ezquerro_practica_android_m08uf2/MainActivity.java
+++ app/src/main/java/com/penjat/jarrega_ezquerro_practica_android_m08uf2/MainActivity.java
@@ -2,13 +2,45 @@ package com.penjat.jarrega_ezquerro_practica_android_m08uf2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * Barra acció
+ * menú
+ * pantalla de benvinguda
+ * Sistema de pistes (seqüencia de preguntes) Firebase
+ * Musica de fons (opció de parar-la)
+ * Sons de encert i error
+ * Bones practiques de programacio multimedia (sons simultanis)
+ * Animacions amb descriptors XML
+ * Vista personalitzada i canvas
+ * Serveis (IntentService i subclasses Service)
+ * Propis fils (gestio basant-se amb prioritat)
+ */
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Button btJugar;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btJugar = findViewById(R.id.btPlay);
+        btJugar.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, HangedActivity.class);
+        startActivity(intent);
+    }
+
+
+
+
+
 }
